6b6dc83653cdb552187e956b7c6e29dd91af1dae
unknown <boom.rang1234567890@gmail.com>
2022-05-19 12:28:59 +0700

    all clean code ver.1

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -2,6 +2,7 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Database from "@ioc:Adonis/Lucid/Database";
 import User from "App/Models/User";
 export default class UsersController {
+  
   public async get(ctx: HttpContextContract) {
     const users = await Database.from("users").orderBy("id", "asc");
     return users;
@@ -9,32 +10,29 @@ export default class UsersController {
 
   public async create(ctx: HttpContextContract) {
     const user = new User();
-    const u_name = ctx.request.input("username");
+    
+    user.username = ctx.request.input("username");
+    user.email = ctx.request.input("email");
+    user.fname = ctx.request.input("fname");
+    user.lname = ctx.request.input("lname");
+    user.avatar = ctx.request.input("avatar");
+    
     const users = await Database.from("users");
-    console.log(users);
-    if (u_name == "") {
+    if (user.username == "") {
       return {
         status: "error",
         message: "username require",
       };
     }
-    const u_find = users.find((e) => e.username == u_name);
+    const u_find = users.find((e) => e.username == user.username);
     console.log(u_find);
     if (u_find == undefined) {
-      // console.log(users[i].username)
-      user.username = ctx.request.input("username");
-      user.email = ctx.request.input("email");
-      user.fname = ctx.request.input("fname");
-      user.lname = ctx.request.input("lname");
-      user.avatar = ctx.request.input("avatar");
       await user.save();
-      // console.log(user.$isPersisted);
       return {
         status: "ok",
         message: "created",
       };
     } else {
-      // console.log("Error");
       return {
         status: "error",
         message: "this username has already exits",
@@ -51,12 +49,18 @@ export default class UsersController {
       message: "Deleted",
     };
   }
+ 
   public async update(ctx: HttpContextContract) {
     const users = await Database.from("users");
     const id = ctx.request.input("id");
     const user = await User.findOrFail(id);
 
+    user.email = ctx.request.input("email");
+    user.fname = ctx.request.input("fname");
+    user.lname = ctx.request.input("lname");
+    user.avatar = ctx.request.input("avatar");
     user.username = ctx.request.input("username");
+    
     if (user.username == "") {
       return {
         status: "error",
@@ -67,11 +71,6 @@ export default class UsersController {
     console.log(u_find);
     if (u_find == undefined) {
       // console.log(users[i].username)
-      user.email = ctx.request.input("email");
-      user.fname = ctx.request.input("fname");
-      user.lname = ctx.request.input("lname");
-      user.avatar = ctx.request.input("avatar");
-
       await user.save();
       return {
         status: "ok",
